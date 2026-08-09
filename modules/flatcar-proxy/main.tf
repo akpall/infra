6be3f5dde01bd4c61887fb0f057a850d646b97d6
Allen-Kristjan Päll <akpall@fst.ee>
2026-08-10 00:35:08 +0300

    flatcar-proxy: output ignition config

--- modules/flatcar-proxy/main.tf
+++ modules/flatcar-proxy/main.tf
@@ -1,3 +1,24 @@
+data "ignition_config" "flatcar-proxy" {
+  users = [
+    data.ignition_user.core.rendered,
+  ]
+}
+
+data "ignition_user" "core" {
+  name                = "core"
+  ssh_authorized_keys = var.ssh_authorized_keys
+}
+
+output "ignition_config" {
+  value = data.ignition_config.flatcar-proxy.rendered
+}
+
+resource "local_file" "flatcar-proxy" {
+  file_permission = "0600"
+  filename        = "${var.files_dir}/flatcar-proxy.json"
+  content         = data.ignition_config.flatcar-proxy.rendered
+}
+
 terraform {
   required_providers {
     ignition = {
@@ -6,21 +27,8 @@ terraform {
   }
 }
 
-data "ignition_user" "core" {
-  name                = "core"
-  ssh_authorized_keys = var.ssh_authorized_keys
-}
+variable "files_dir" {}
 
 variable "ssh_authorized_keys" {
   type = list(string)
-}
-
-data "ignition_config" "flatcar-proxy" {
-  users = [
-    data.ignition_user.core.rendered,
-  ]
-}
-
-output "ignition_config" {
-  value = data.ignition_config.flatcar-proxy.rendered
 }

7a7ac44aa11925677e0152c17b55cb055b94ae60
Allen-Kristjan Päll <akpall@fst.ee>
2026-08-10 00:30:55 +0300

    ssh-keys: trimspace output

--- modules/ssh-keys/main.tf
+++ modules/ssh-keys/main.tf
@@ -1,10 +1,10 @@
 output "terragrunt_private_key" {
   sensitive = true
-  value     = resource.tls_private_key.terragrunt.private_key_pem
+  value     = trimspace(resource.tls_private_key.terragrunt.private_key_pem)
 }
 
 output "terragrunt_public_key" {
-  value = resource.tls_private_key.terragrunt.public_key_openssh
+  value = trimspace(resource.tls_private_key.terragrunt.public_key_openssh)
 }
 
 resource "tls_private_key" "terragrunt" {

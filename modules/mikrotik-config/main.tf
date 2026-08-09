51356e9ea2511a9bf16ada8ab630586f0b5c2749
Allen-Kristjan Päll <akpall@fst.ee>
2026-08-09 19:03:18 +0300

    mikrotik-config: add lan bridge and ether interfaces

--- modules/mikrotik-config/main.tf
+++ modules/mikrotik-config/main.tf
@@ -1,3 +1,7 @@
+locals {
+  ports = ["ether1", "ether2", "ether3", "ether4", "ether5"]
+}
+
 provider "routeros" {
   ca_certificate = var.ca_certificate
   password       = var.password
@@ -5,9 +9,20 @@ provider "routeros" {
   hosturl        = var.hosturl
 }
 
-resource "routeros_interface_bridge" "bridge" {
+resource "routeros_interface_bridge" "lan" {
   auto_mac = true
-  name     = "bridge"
+  name     = "lan"
+}
+
+resource "routeros_interface_bridge_port" "ether" {
+  for_each  = toset(local.ports)
+  bridge    = "lan"
+  interface = each.key
+}
+
+resource "routeros_ipv6_address" "lan_ipv6_address" {
+  address   = var.router_ip
+  interface = "lan"
 }
 
 terraform {
--- modules/mikrotik-config/variables.tf
+++ modules/mikrotik-config/variables.tf
@@ -1,4 +1,5 @@
 variable "ca_certificate" {}
 variable "hosturl" {}
 variable "password" {}
+variable "router_ip" {}
 variable "username" {}

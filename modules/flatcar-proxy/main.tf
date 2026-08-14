3a7d617e04d7fcf1e88e0ea81ebb20fe5d7bdfcf
Allen-Kristjan Päll <akpall@fst.ee>
2026-08-14 20:57:07 +0300

    flatcar-proxy: define static network

--- modules/flatcar-proxy/main.tf
+++ modules/flatcar-proxy/main.tf
@@ -2,6 +2,9 @@ data "ignition_config" "flatcar-proxy" {
   users = [
     data.ignition_user.core.rendered,
   ]
+  files = [
+    data.ignition_file.static-network.rendered,
+  ]
 }
 
 data "ignition_user" "core" {
@@ -32,4 +35,31 @@ variable "ignition_dir" {}
 
 variable "ssh_authorized_keys" {
   type = list(string)
+}
+
+data "ignition_file" "static-network" {
+  path = "/etc/systemd/network/00-eth0.network"
+  content {
+    content = <<-EOT
+      [Match]
+      Name=eth0
+
+      [Network]
+      Address=193.40.103.107/24
+      Gateway=193.40.103.1
+      DHCP=ipv6
+      IPv6AcceptRA=yes
+
+      [DHCPv6]
+      DUIDType=uuid
+      DUIDRawData=4e:b7:b9:ab:4b:9f:4a:5f:bf:5d:bd:b8:3a:04:ce:de
+      PrefixDelegationHint=2001:bb8:4008:20c0::/60
+
+      [DHCPPrefixDelegation]
+      UplinkInterface=:self
+      SubnetId=0
+      Token=::1
+      Announce=yes
+    EOT
+  }
 }
